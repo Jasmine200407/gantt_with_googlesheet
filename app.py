e61ee68a17401edbd5421de431275ae191f0afc0
Jasmine200407 <162100046+Jasmine200407@users.noreply.github.com>
2025-04-14 18:08:26 +0800

    Update app.py

--- app.py
+++ app.py
@@ -121,4 +121,4 @@ from flask import render_template
 def home():
     return render_template('index.html')
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000))) # deploy on render
