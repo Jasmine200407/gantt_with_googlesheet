a63d82960f830ff157d60104fa95b27f09722208
Jasmine200407 <162100046+Jasmine200407@users.noreply.github.com>
2025-04-14 19:10:35 +0800

    Update app.py

--- app.py
+++ app.py
@@ -138,4 +138,4 @@ def home():
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 10000))  # Render 預設是 10000
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port,, debug=True)
