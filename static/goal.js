be6a5f2fd1a5ded4a1c112d9963efc66c4cff3c4
Jasmine200407 <162100046+Jasmine200407@users.noreply.github.com>
2025-04-21 12:38:13 +0800

    Update goal.js

--- static/goal.js
+++ static/goal.js
@@ -19,7 +19,7 @@ function formatDate(date) {
 }
 
 function getDaysBetween(start, end) {
-    return Math.round((end - start) / (1000 * 60 * 60 * 24));
+  return Math.floor((end - start) / (1000 * 60 * 60 * 24)) + 1;
 }
 
 function todayPosition(start, end) {
